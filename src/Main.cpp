45babec25eee7a99174870d9cef5b1f2f7c87d99
wangao23 <oliverdebox@163.com>
2023-09-03 22:56:35 +0800

    fix:delete and insert bugs
    Signed-off-by: wangao23 <oliverdebox@163.com>

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,31 @@
-#include "Bplus.h"
+#include <cstdint>
 #include <iostream>
 #include <ostream>
 
-int main() {
+#include "Bplus.h"
 
-  BPlusTree<int> *intTree = new BPlusTree<int>();
+void create_bplus_tree_man(BPlusTree<int> *&intTree) {
+  if (intTree != nullptr) {
+    delete intTree;
+  }
+  intTree = new BPlusTree<int>();
+  intTree->setDegree();
+  int value;
+  uint64_t key;
+  while (true) {
+    std::cout << "please enter key and value ( exit by 0 0) :";
+    std::cin >> key >> value;
+    if (key == 0 && value == 0) {
+      break;
+    }
+    intTree->insert(key, value);
+  }
+  intTree->print();
+}
+
+int main() {
+  BPlusTree<int> *intTree = nullptr;
   std::vector<uint64_t> vec;
-  std::vector<uint64_t> searchVec;
   while (true) {
     std::cout << "-------------------------------" << std::endl;
     std::cout << "Welcome to use B+ tree!" << std::endl;
@@ -21,92 +40,96 @@ int main() {
     std::cout << "9. searchRange , enter start , end " << std::endl;
     std::cout << "10. search , enter key" << std::endl;
     std::cout << "-------------------------------" << std::endl;
-    int funcNum;
+    int funcNum = -1;
     std::cin >> funcNum;
 
     switch (funcNum) {
-    case 1:
-      std::cout << "sorry, the func is not avaliable." << std::endl;
-      break;
-    case 2:
-      if (intTree->start != nullptr) {
-        // 先删除该树
-        delete (intTree);
-      }
-      intTree = new BPlusTree<int>();
-      intTree->setDegree();
-      createTestInt(intTree);
-      // createTest2(intTree);
-      intTree->print();
-      break;
-    case 3:
-      std::cout << "sorry, the func is not avaliable." << std::endl;
-      break;
-    case 4:
-      std::cout << "sorry, the func is not avaliable." << std::endl;
-      break;
-    case 5:
-      intTree->print();
-      break;
-    case 6:
-      if (checkNotCreate(intTree)) {
+      case 1:
+        create_bplus_tree_man(intTree);
         break;
-      }
-      vec.clear();
-      std::cout << "please enter the key that you want to delete" << std::endl;
-      int key;
-      std::cin >> key;
-      intTree->remove(vec, key);
-      intTree->print();
-      break;
-    case 7:
-      exit(0);
-      break;
-    case 8:
-      system("clear");
-      break;
-    case 9:
-      if (checkNotCreate(intTree)) {
+      case 2:
+        if (intTree != nullptr) {
+          delete intTree;
+        }
+        intTree = new BPlusTree<int>();
+        intTree->setDegree();
+        // createTestInt2(intTree);
+        createTestInt1(intTree);
+        intTree->print();
         break;
-      }
-      int start, end;
-      std::cin >> start >> end;
-      searchVec.clear();
-      searchVec = intTree->searchRange(start, end);
-      for (auto item : searchVec) {
-        std::cout << item << " ";
-      }
-      std::cout << std::endl;
-      break;
-    case 10:
-      if (checkNotCreate(intTree)) {
+      case 3:
+        std::cout << "sorry, the func is not avaliable." << std::endl;
         break;
-      }
-      int key2;
-      std::cin >> key2;
-      intTree->optimizedSearch(key2);
-      break;
-    default:
-      break;
+      case 4:
+        std::cout << "sorry, the func is not avaliable." << std::endl;
+        break;
+      case 5:
+        if (checkNotCreate(intTree)) {
+          break;
+        }
+        intTree->print();
+        break;
+      case 6:
+        if (checkNotCreate(intTree)) {
+          break;
+        }
+        vec.clear();
+        std::cout << "please enter the key that you want to delete :";
+        int key6;
+        std::cin >> key6;
+        intTree->remove(vec, key6);
+        if (!(intTree->start->next->keys.empty())) {
+          intTree->print();
+        }
+        std::cout << std::endl;
+        break;
+      case 7:
+        exit(0);
+        break;
+      case 8:
+        system("clear");
+        break;
+      case 9:
+        if (checkNotCreate(intTree)) {
+          break;
+        }
+        int start, end;
+        std::cin >> start >> end;
+        intTree->searchRange(start, end);
+        std::cout << std::endl;
+        break;
+      case 10:
+        if (checkNotCreate(intTree)) {
+          break;
+        }
+        std::cout << "please enter key :";
+        int key2;
+        std::cin >> key2;
+        intTree->optimizedSearch(key2);
+        std::cout << std::endl;
+        break;
+      default:
+        break;
     }
   }
   return 0;
 }
 
 bool checkNotCreate(BPlusTree<int> *root) {
-  if (root->start == nullptr) {
+  if (root == nullptr) {
     std::cout << "please retry again after creating a BPlusTree!" << std::endl;
     return true;
   }
   return false;
 }
+
 void createTestInt2(BPlusTree<int> *intTree) {
-  for (int i = 0; i < 100; ++i) {
+  for (int i = 1; i < 8; ++i) {
     intTree->insert(i, i * 2);
   }
 }
 
-void createTestInt(BPlusTree<int> *intTree) {
+void createTestInt1(BPlusTree<int> *intTree) {
   intTree->insert(5, 100);
   intTree->insert(3, 200);
   intTree->insert(7, 300);
@@ -118,46 +141,33 @@ void createTestInt(BPlusTree<int> *intTree) {
   intTree->insert(7, 400);
   intTree->insert(5, 100);
   intTree->insert(3, 200);
-  // intTree->insert(7, 300);
-  // intTree->insert(4, 400);
-  // intTree->insert(8, 300);
-  // intTree->insert(9, 400);
-  // intTree->insert(10, 400);
-  // intTree->insert(6, 400);
-  // intTree->insert(6, 400);
-  // intTree->insert(7, 400);
-  // intTree->insert(5, 100);
-  // intTree->insert(3, 200);
-  // intTree->insert(7, 300);
-  // intTree->insert(4, 400);
-  // intTree->insert(8, 300);
-  // intTree->insert(9, 400);
-  // intTree->insert(6, 400);
-  // intTree->insert(6, 400);
-  // intTree->insert(7, 400);
-  // intTree->insert(5, 100);
-  // intTree->insert(3, 200);
-  // intTree->insert(7, 300);
-  // intTree->insert(4, 400);
-  // intTree->insert(8, 300);
-  // intTree->insert(9, 400);
-  // intTree->insert(10, 400);
-  // intTree->insert(6, 400);
-  // intTree->insert(6, 400);
-  // intTree->insert(7, 400);
-  // intTree->insert(5, 100);
-  // intTree->insert(3, 200);
-  // intTree->insert(7, 300);
-  // intTree->insert(4, 400);
-  // intTree->insert(8, 300);
-  // intTree->insert(9, 400);
-  // intTree->insert(6, 400);
-  // intTree->insert(6, 400);
-  // intTree->insert(7, 400);
-  // intTree->insert(5, 100);
-  // intTree->insert(3, 200);
-  // intTree->insert(7, 300);
-  // intTree->insert(4, 400);
-  // intTree->insert(8, 300);
-  // intTree->insert(9, 400);
+  intTree->insert(7, 300);
+  intTree->insert(4, 400);
+  intTree->insert(8, 300);
+  intTree->insert(9, 400);
+  intTree->insert(10, 400);
+  intTree->insert(6, 400);
+  intTree->insert(6, 400);
+  intTree->insert(7, 400);
+  intTree->insert(5, 100);
+  intTree->insert(5, 100);
+  intTree->insert(3, 200);
+  intTree->insert(7, 300);
+  intTree->insert(4, 400);
+  intTree->insert(8, 300);
+  intTree->insert(9, 400);
+  intTree->insert(6, 400);
+  intTree->insert(6, 400);
+  intTree->insert(7, 400);
+  intTree->insert(5, 100);
+  intTree->insert(3, 200);
+  intTree->insert(7, 300);
+  intTree->insert(4, 400);
+  intTree->insert(8, 300);
+  intTree->insert(9, 400);
+  intTree->insert(10, 400);
+  intTree->insert(6, 400);
+  intTree->insert(6, 400);
+  intTree->insert(7, 400);
+  intTree->insert(5, 100);
 }

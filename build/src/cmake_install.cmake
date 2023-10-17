e4ecc8ddd8228e750c958be0c2dc3b9eb5cd99b9
oliverdebox@163.com <oliver@LAPTOP-Q7N2E2ED>
2023-10-17 12:08:47 +0800

    gtest
    Signed-off-by: oliverdebox@163.com <oliver@LAPTOP-Q7N2E2ED>

--- build/src/cmake_install.cmake
+++ build/src/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/oliver/Developer/bPlusProject/src
+# Install script for directory: /home/oliver/cplus/training-plan/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)

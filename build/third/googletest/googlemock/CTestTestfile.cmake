044ef87689c165674dd0a1d5d6a004b6bbcf08f3
oliverdebox@163.com <oliver@LAPTOP-Q7N2E2ED>
2023-10-17 12:53:06 +0800

    update .gitignore

--- build/third/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/oliver/cplus/training-plan/third/googletest/googlemock
-# Build directory: /home/oliver/cplus/training-plan/build/third/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/third/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/oliver/cplus/training-plan/third/googletest/googletest
-# Build directory: /home/oliver/cplus/training-plan/build/third/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
